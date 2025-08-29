a28a72b6c9479fbf16fa82e8990ef9d7b57e6d95
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2025-08-29 21:09:37 +0200

    Added /logout endpoint

--- app/resources/user.py
+++ app/resources/user.py
@@ -1,12 +1,14 @@
 from flask.views import MethodView
 from flask_smorest import abort, Blueprint
 from passlib.hash import pbkdf2_sha256 as sha256
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt
 from sqlalchemy.exc import SQLAlchemyError
 
 from app.extensions import db
 from app.models import UserModel
 from app.schemas import UserSchema
+
+from blocklist import BLOCKLIST
 
 blp = Blueprint("user", __name__, description="Operations on users")
 
@@ -29,6 +31,26 @@ class UserRegister(MethodView):
             abort(500, message="An error occurred while creating the user.")
 
         return {"message": "User created successfully."}, 201
+        
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(UserModel.email == user_data['email']).first()
+
+        if user and sha256.verify(user_data['password'], user.password):
+            access_token = create_access_token(identity=str(user.id), fresh=True)
+            return {"access_token": access_token}, 200
+
+        abort(401, message="Invalid credentials.")
+
+@blp.route("/logout")
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()['jti']
+        BLOCKLIST.add(jti)
+        return {"message": "Successfully logged out."}, 200
     
 @blp.route("/user/<int:user_id>")
 class User(MethodView):
@@ -47,15 +69,3 @@ class User(MethodView):
             abort(500, message="An error occurred while deleting the user.")
 
         return {"message": "User deleted."}, 200
-    
-@blp.route("/login")
-class UserLogin(MethodView):
-    @blp.arguments(UserSchema)
-    def post(self, user_data):
-        user = UserModel.query.filter(UserModel.email == user_data['email']).first()
-
-        if user and sha256.verify(user_data['password'], user.password):
-            access_token = create_access_token(identity=str(user.id), fresh=True)
-            return {"access_token": access_token}, 200
-
-        abort(401, message="Invalid credentials.")
