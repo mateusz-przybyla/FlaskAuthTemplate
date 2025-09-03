3bcd9708cb7c9fe1b2f9afc63d1d6b3f7aca8fd5
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2025-09-03 18:39:20 +0200

    Add pytest.ini file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# add the app directory to the sys.path for imports
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 import pytest
 from app import create_app
 from app.extensions import db
