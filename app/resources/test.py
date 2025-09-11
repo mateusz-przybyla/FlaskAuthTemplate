4279fb0290d1edb30b8207052d2de1866b3dfb85
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2025-09-11 21:17:45 +0200

    Change response key msg → message in test endpoints

--- app/resources/test.py
+++ app/resources/test.py
@@ -7,16 +7,16 @@ blp = Blueprint("test", __name__, description="Test endpoints")
 @blp.route("/guest")
 class TestGuestEndpoint(MethodView):
     def get(self):
-        return {"msg": "This endpoint is open to everyone."}
+        return {"message": "This endpoint is open to everyone."}
 
 @blp.route("/protected")
 class TestAuthEndpoint(MethodView):
     @jwt_required()
     def get(self):
-        return {"msg": "This is a protected endpoint."}
+        return {"message": "This is a protected endpoint."}
     
 @blp.route("/fresh-protected")
 class TestFreshAuthEndpoint(MethodView):
     @jwt_required(fresh=True)
     def get(self):
-        return {"msg": "This is a protected endpoint. You used a fresh token to access it."}
+        return {"message": "This is a protected endpoint. You used a fresh token to access it."}
